1ea38368d9c3facb1b8971c49cb0a8a90be13e06
Marco Brioschi <marco.brioschi@gmail.com>
2015-09-12 23:14:30 +0200

    Setup test and TravisCI integration

---
+++ src/main/java/biz/marcobrioschi/lmtest/PlaceholderToRemove.java
@@ -0,0 +1,6 @@
+package biz.marcobrioschi.lmtest;
+
+// FIXME: Empty class for git commit
+public class PlaceholderToRemove {
+	
+}
---
+++ src/test/java/biz/marcobrioschi/lmtest/PlaceholderTestFail.java
@@ -0,0 +1,15 @@
+package biz.marcobrioschi.lmtest;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+//FIXME: remove me
+public class PlaceholderTestFail {
+
+	@Test
+	public void TravisCI_whenTheUnitTestFail() {
+		fail("Check TravisCI");
+	}
+
+}
