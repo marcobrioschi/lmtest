c6a413f93900396066da0005e6a5f2d5c30ccc62
Marco Brioschi <marco.brioschi@gmail.com>
2015-09-15 00:52:58 +0200

    Added test

--- src/test/java/biz/marcobrioschi/lmtest/util/TaxMathTest.java
+++ src/test/java/biz/marcobrioschi/lmtest/util/TaxMathTest.java
@@ -14,14 +14,18 @@ public class TaxMathTest {
 		assertEquals(0.10, TaxMath.calculateRoundedTaxValue(0.7, 0.1), 0.0);
 		assertEquals(0.10, TaxMath.calculateRoundedTaxValue(0.8, 0.1), 0.0);
 		assertEquals(0.10, TaxMath.calculateRoundedTaxValue(0.9, 0.1), 0.0);
+		assertEquals(0.10, TaxMath.calculateRoundedTaxValue(0.95, 0.1), 0.0);
 		assertEquals(0.10, TaxMath.calculateRoundedTaxValue(1.0, 0.1), 0.0);
 		
+		assertEquals(0.15, TaxMath.calculateRoundedTaxValue(1.05, 0.1), 0.0);
 		assertEquals(0.15, TaxMath.calculateRoundedTaxValue(1.1, 0.1), 0.0);
 		assertEquals(0.15, TaxMath.calculateRoundedTaxValue(1.2, 0.1), 0.0);
 		assertEquals(0.15, TaxMath.calculateRoundedTaxValue(1.3, 0.1), 0.0);
 		assertEquals(0.15, TaxMath.calculateRoundedTaxValue(1.4, 0.1), 0.0);
+		assertEquals(0.15, TaxMath.calculateRoundedTaxValue(1.45, 0.1), 0.0);
 		assertEquals(0.15, TaxMath.calculateRoundedTaxValue(1.5, 0.1), 0.0);
 		
+		assertEquals(0.20, TaxMath.calculateRoundedTaxValue(1.55, 0.1), 0.0);
 		assertEquals(0.20, TaxMath.calculateRoundedTaxValue(1.6, 0.1), 0.0);
 		assertEquals(0.20, TaxMath.calculateRoundedTaxValue(1.7, 0.1), 0.0);
 		assertEquals(0.20, TaxMath.calculateRoundedTaxValue(1.8, 0.1), 0.0);
